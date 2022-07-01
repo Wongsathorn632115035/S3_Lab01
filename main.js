43de82d67107fbc09638c7e7289e58e9c31f6713
markwst <wongsathorn_w@cmu.ac.th>
2022-07-01 10:23:44 +0700

    Q 6.7

--- main.js
+++ main.js
@@ -2,18 +2,36 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Shoes',
-            info: 'Welcome to our store.',
+            info: 'Hello, Welcome to my store.',
             image: './assets/images/socks_green.jpg',
             web: 'https://www.camt.cmu.ac.th/index.php/th/',
             inStock: true,
             inventory: 100,
+            onSale: true,
             details: ['50% cotton', '30% wool', '20% polyester '],
             variants: [
-                { id: 2234, color: 'green' },
-                { id: 2235, color: 'blue' }
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' }
 
             ],
-            size: ['S', 'M', 'L']
+            size: ['S', 'M', 'L'],
+            cart: 0,
+        }
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        updateImage(variantImage) {
+            this.image = variantImage
+        },
+        changeStatus() {
+            if (this.inventory == 100) {
+                this.inventory = 0
+            } else {
+                this.inventory = 100
+            }
+
         }
     }
 })
